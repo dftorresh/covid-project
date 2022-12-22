7efcc9a23994281c84544a4c6c7f483b53378b5b
dftorresh <dafetohe2@gmail.com>
2022-12-22 03:12:29 +0000

    Variables, Resource group, Providers and output files created

---
+++ Terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "resource_group_location" {
+  default     = "eastus"
+  description = "Location of the resource group."
+}
+
+variable "resource_group_name" {
+  default     = "rg_covid_project_28"
+  description = "Resource group where all the other resource will be created."
+}
