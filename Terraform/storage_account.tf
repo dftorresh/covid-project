163033314f520d78a685c53b5f393158a56e304d
dftorres <dafetohe2@gmail.com>
2022-12-22 21:09:07 -0500

    Storage account creation file added

---
+++ Terraform/storage_account.tf
@@ -0,0 +1,27 @@
+resource "azurerm_storage_account" "sa" {
+    name = "covid28datadl"
+    resource_group_name = azurerm_resource_group.rg.name
+    location = var.location
+    account_tier = "Standard"
+    account_replication_type = "LRS"
+    account_kind = "StorageV2"
+    is_hns_enabled = true
+}
+
+resource "azurerm_storage_container" "raw_data_container" {
+    name = var.raw_data_container_name
+    storage_account_name = azurerm_storage_account.sa.name
+    container_access_type = "blob"
+}
+
+resource "azurerm_storage_container" "processed_data_container" {
+    name = var.processed_data_container_name
+    storage_account_name = azurerm_storage_account.sa.name
+    container_access_type = "blob"
+}
+
+resource "azurerm_storage_container" "lookup_data_container" {
+    name = var.lookup_data_container_name
+    storage_account_name = azurerm_storage_account.sa.name
+    container_access_type = "blob"
+}
--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,9 +1,24 @@
 variable "location" {
   default     = "eastus"
-  description = "Location of the resource group."
+  description = "Location to be used for all the resources."
 }
 
 variable "resource_group_name" {
   default     = "rg_covid_project_28"
-  description = "Resource group where all the other resource will be created."
+  description = "Resource group where all the other resources will be created."
+}
+
+variable "raw_data_container_name" {
+  default     = "raw"
+  description = "Container where raw data will be stored"
+}
+
+variable "processed_data_container_name" {
+  default     = "processed"
+  description = "Container where the data will be stored after processing"
+}
+
+variable "lookup_data_container_name" {
+  default     = "lookup"
+  description = "Container containing some data used to enrich the covid dataset"
 }
